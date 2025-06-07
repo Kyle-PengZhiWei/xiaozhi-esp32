85a19ee95c8b2d044b0b6da0845ce8ef78c0cfd2
Kyle Peng <pengzhiwei9.23@qq.com>
2025-06-07 16:48:52 +0800

    为bread-compact-wifi板子引脚配置文件添加注释

--- main/boards/bread-compact-wifi/config.h
+++ main/boards/bread-compact-wifi/config.h
@@ -3,6 +3,7 @@
 
 #include <driver/gpio.h>
 
+// 定义音频输入和输出的采样率
 #define AUDIO_INPUT_SAMPLE_RATE  16000
 #define AUDIO_OUTPUT_SAMPLE_RATE 24000
 
@@ -10,7 +11,7 @@
 #define AUDIO_I2S_METHOD_SIMPLEX
 
 #ifdef AUDIO_I2S_METHOD_SIMPLEX
-
+// 简工模式下，定义音频 I2S 接口的 GPIO 引脚
 #define AUDIO_I2S_MIC_GPIO_WS   GPIO_NUM_4
 #define AUDIO_I2S_MIC_GPIO_SCK  GPIO_NUM_5
 #define AUDIO_I2S_MIC_GPIO_DIN  GPIO_NUM_6
@@ -19,7 +20,7 @@
 #define AUDIO_I2S_SPK_GPIO_LRCK GPIO_NUM_16
 
 #else
-
+// 非简工模式下，定义音频 I2S 接口的 GPIO 引脚
 #define AUDIO_I2S_GPIO_WS GPIO_NUM_4
 #define AUDIO_I2S_GPIO_BCLK GPIO_NUM_5
 #define AUDIO_I2S_GPIO_DIN  GPIO_NUM_6
@@ -27,17 +28,19 @@
 
 #endif
 
-
+// 定义板载 LED 和按钮的 GPIO 引脚
 #define BUILTIN_LED_GPIO        GPIO_NUM_48
 #define BOOT_BUTTON_GPIO        GPIO_NUM_0
 #define TOUCH_BUTTON_GPIO       GPIO_NUM_47
 #define VOLUME_UP_BUTTON_GPIO   GPIO_NUM_40
 #define VOLUME_DOWN_BUTTON_GPIO GPIO_NUM_39
 
+// 定义显示屏幕的 I2C 接口引脚和尺寸
 #define DISPLAY_SDA_PIN GPIO_NUM_41
 #define DISPLAY_SCL_PIN GPIO_NUM_42
 #define DISPLAY_WIDTH   128
 
+// 根据配置选择 OLED 屏幕的类型和尺寸
 #if CONFIG_OLED_SSD1306_128X32
 #define DISPLAY_HEIGHT  32
 #elif CONFIG_OLED_SSD1306_128X64
@@ -49,9 +52,9 @@
 #error "未选择 OLED 屏幕类型"
 #endif
 
+// 定义屏幕显示是否翻转
 #define DISPLAY_MIRROR_X true
 #define DISPLAY_MIRROR_Y true
-
 
 // A MCP Test: Control a lamp
 #define LAMP_GPIO GPIO_NUM_18
